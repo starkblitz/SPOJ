57b006657eed892a4dab2529ebd2e91aa7af2a3c
starkblitz <39380418+starkblitz@users.noreply.github.com>
2018-09-30 10:14:05 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	short int t;
+	cin>>t;
+	while(t!=42)
+	{
+		cout<<t<<endl;
+		cin>>t;
+	}
+	return 0;
+}
